5dfabfe7cdaf8f277a4125b8c840dca18d112144
kimtae55 <taehyo97@gmail.com>
2021-02-07 00:26:17 -0500

    Model1 fixed and working
    To run:
    python test_theta.py num_rng_seed
    python test_data.py num_rng_seed
    python test_fdr.py num_rng_seed

---
+++ Model1/Dim3/test_data.py
@@ -0,0 +1,116 @@
+import numpy as np
+import os
+import shutil
+import fnmatch
+import math
+import matplotlib.pyplot as plt
+import torch
+import sys
+
+class Data:
+    VOXEL_SIZE = 30
+
+    def __init__(self, rng_seed):
+        self.v = np.empty((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE))
+        self.theta = np.empty((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE))
+        self.rng_seed = rng_seed
+        self.SAVE_PATH = os.path.join(os.getcwd(), '../data/' + str(rng_seed) + '/voxels')
+        self.GROUP0_PATH = os.path.join(self.SAVE_PATH, '0')
+        self.GROUP1_PATH = os.path.join(self.SAVE_PATH, '1')
+        self.ZIP_PATH = os.path.join(self.SAVE_PATH, 'voxels.zip')
+
+    @staticmethod
+    def load(path):
+        x = np.loadtxt(path).reshape((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE))
+        return x
+
+    def loadTheta(self, path):
+        self.theta = torch.load(path).cpu().numpy()
+
+    def generate_v(self, p_l, mu_l, sigma_l, L, subjects, groups):
+        # use equation (4) to generate n voxels for j subjects from k groups
+        # save the voxels in .txt into k folders
+        # will generate voxels.zip
+        mu, sigma = 0, 1
+
+        for gn in range(groups):
+            directory = os.path.join(self.SAVE_PATH, str(gn) + '/')
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            # compute voxels
+            for sn in range(subjects):
+                for i in range(Data.VOXEL_SIZE):
+                    for j in range(Data.VOXEL_SIZE):
+                        for k in range(Data.VOXEL_SIZE):
+                            sum = 0
+                            for l in range(L):
+                                sum = sum + p_l*np.random.normal(gn*mu_l,sigma_l**2)
+                            self.v[i][j][k] = (1-self.theta[i][j][k])*np.random.normal(mu,sigma) \
+                                              + self.theta[i][j][k]*sum
+                # save datum to correct folder
+                filename = 'k=' + str(gn) + '_j=' + str(sn) + '.txt'
+                savepath = os.path.join(directory, filename)
+                with open(savepath, 'w') as outfile:
+                    outfile.write('# Array shape: {0}\n'.format(self.v.shape))
+                    outfile.write('# H = 1: {0}\n'.format(np.count_nonzero(self.v)))
+                    for data_slice in self.v:
+                        np.savetxt(outfile, data_slice, fmt='%-7.2f')
+                        outfile.write('# New z slice\n')
+        # zip folder
+        shutil.make_archive(self.SAVE_PATH, 'zip', self.SAVE_PATH)
+
+    def generate_x(self, subjects):
+        # load data from zipped folder
+        # use simulation model (1) to generate x statistics
+        # save into txt
+        if not os.path.exists(self.SAVE_PATH) and os.path.exists(self.ZIP_PATH):
+            shutil.unpack_archive(self.ZIP_PATH, self.SAVE_PATH, 'zip')
+
+        v_0 = np.zeros((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE))
+        for i, filename in enumerate(os.listdir(self.GROUP0_PATH)):
+            if filename.endswith(".txt"):
+                v_0 = v_0 + np.loadtxt(os.path.join(self.GROUP0_PATH,filename)).reshape((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE))
+
+        v_1 = np.zeros((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE))
+        for i, filename in enumerate(os.listdir(self.GROUP1_PATH)):
+            if filename.endswith(".txt"):
+                v_1 = v_1 + np.loadtxt(os.path.join(self.GROUP1_PATH,filename)).reshape((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE))
+
+        x = np.zeros((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE))
+        n0 = len(fnmatch.filter(os.listdir(self.GROUP0_PATH), '*.txt'))
+        n1 = len(fnmatch.filter(os.listdir(self.GROUP0_PATH), '*.txt'))
+        for i in range(Data.VOXEL_SIZE):
+            for j in range(Data.VOXEL_SIZE):
+                for k in range(Data.VOXEL_SIZE):
+                    x[i][j][k] = ((1/n0)*v_0[i][j][k] - (1/n1)*v_1[i][j][k]) / (math.sqrt((1/n0)+(1/n1)))
+
+        filename = '../data/' + str(rng_seed) + '/x_val.txt'
+        savepath = os.path.join(os.getcwd(),filename)
+        with open(savepath, 'w') as outfile:
+            outfile.write('# Array shape: {0}\n'.format(x.shape))
+            outfile.write('# H = 1: {0}\n'.format(np.count_nonzero(x)))
+            for data_slice in x:
+                np.savetxt(outfile, data_slice, fmt='%-7.2f')
+                outfile.write('# New z slice\n')
+
+
+
+if __name__ == "__main__":
+    rng_seed = int(sys.argv[1])
+    print("RAND: ", rng_seed)
+    np.random.seed(rng_seed)
+    data = Data(rng_seed)
+    theta_path = os.path.join(os.getcwd(), '../data/' + str(rng_seed) +'/label/label.pt')
+    data.loadTheta(theta_path)
+    data.generate_v(p_l=0.5, mu_l=0.5, sigma_l=1, L=2, subjects=200, groups=2)
+    data.generate_x(subjects=200)
+
+'''
+    x = np.loadtxt(os.path.join('../data/x_valtest.txt')).reshape((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE))
+    for k in range(Data.VOXEL_SIZE):
+        fig = plt.figure()
+        plt.subplot(1, 2, 1)
+        plt.imshow(x[k])
+        plt.title('x_statistic')
+        plt.show()
+'''
---
+++ Model1/Dim3/test_theta.py
@@ -0,0 +1,142 @@
+import numpy as np
+import os
+import math
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from DataUtil import Data
+import sys
+
+class GibbsSampler:
+    def __init__(self, rng_seed):
+        self.white_map = torch.from_numpy(np.indices((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE)).sum(axis=0) % 2)
+        self.white_map = self.white_map.cuda()
+        ones = torch.ones((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE)).cuda()
+        self.black_map = torch.logical_xor(self.white_map, ones).type(torch.FloatTensor)
+        self.black_map = self.black_map.cuda()
+        
+        # RUN FROM INSIDE DIM3 FOLDER
+        self.KERNEL_PATH = os.path.join(os.getcwd(), 'kernel.txt')
+        self.SAVE_DIR = os.path.join(os.getcwd(), '../data/' + str(rng_seed) +'/label')
+        self.LABEL_PATH = os.path.join(self.SAVE_DIR, 'label.pt')
+        self.LABEL_TXT_PATH = os.path.join(self.SAVE_DIR, 'label.txt')
+        self.DISTRIBUTION_PNG = os.path.join(self.SAVE_DIR, 'distribution.png')
+        self.LOG_PATH = os.path.join(self.SAVE_DIR, 'log.txt')
+        return
+
+    def run(self, burn_in, n_iter):
+        # Store P(theta=1) over time
+        probOverTime = torch.tensor(np.zeros(burn_in))
+
+        # initialize labels
+        init_prob = torch.full((Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE), 0.5)
+        label = torch.bernoulli(init_prob).cuda()  # p = 0.5 to choose 1
+        kernel = torch.from_numpy(np.loadtxt(self.KERNEL_PATH).reshape((3,3,3))).float()
+        kernel = torch.unsqueeze(kernel, 0)
+        kernel = torch.unsqueeze(kernel, 0)
+        kernel = kernel.cuda()
+
+        iteration = 0
+        iter_burn = 0
+        while iteration < n_iter:
+            # calculate sum_nn
+            # sum of neighboring pixels is a convolution operation
+            label = torch.unsqueeze(label, 0)
+            label = torch.unsqueeze(label, 0)
+            sum_nn = torch.nn.functional.conv3d(label, kernel, stride=1, padding=1).squeeze()
+
+            # calculate exp_sum_nn
+            beta = 0.8
+            h = -2.5
+            numerator = torch.exp(sum_nn*beta + h)
+            conditional_distribution = numerator / (1 + numerator)
+            # then put it into bernoulli
+            white = torch.bernoulli(conditional_distribution)
+            white = torch.logical_and(white, self.white_map).type(torch.FloatTensor).cuda()
+            #print(white)
+            # save a pre_label 
+            # keep black tiles, replace white tiles with pre_label 
+            # compute sum_nn again for white tiles now
+            # put it into bernoulli, get white_labels
+            # now merge the white label with black label 
+            # (how to merge using gpu?) --> logical_or
+            # calculate sum_nn
+            # sum of neighboring pixels is a convolution operation
+            # we have updated the white labels, so merge it with label
+            black_label = torch.logical_and(label.squeeze(), self.black_map).type(torch.FloatTensor).cuda()
+            label = torch.logical_or(black_label, white).type(torch.FloatTensor).cuda()
+            label = torch.unsqueeze(label, 0)
+            label = torch.unsqueeze(label, 0)
+            sum_nn = torch.nn.functional.conv3d(label, kernel, stride=1, padding=1).squeeze()
+            
+            # calculate exp_sum_nn
+            numerator = torch.exp(sum_nn*beta + h)
+            conditional_distribution = numerator / (1 + numerator)
+            # then put it into bernoulli
+            black = torch.bernoulli(conditional_distribution)
+            black = torch.logical_and(black, self.black_map).type(torch.FloatTensor).cuda()
+            label = torch.logical_or(black, white).type(torch.FloatTensor).cuda()
+            
+            if iter_burn < burn_in:
+                probOverTime[iter_burn] = torch.count_nonzero(label) / (Data.VOXEL_SIZE * Data.VOXEL_SIZE * Data.VOXEL_SIZE)
+                iter_burn = iter_burn + 1
+                #print("burn in: ", iter_burn)
+            else:
+                iteration += 1
+        if os.path.isdir(self.SAVE_DIR):
+            torch.save(label, self.LABEL_PATH)
+            # Save final signal_file
+            with open(self.LABEL_TXT_PATH, 'w') as outfile:
+                outfile.write('# H = 1: {0}\n'.format(np.count_nonzero(label.cpu().numpy())))
+                for data_slice in label.cpu().numpy():
+                    np.savetxt(outfile, data_slice, fmt='%-8.4f')
+                    outfile.write('# New z slice\n')
+        else:
+            os.makedirs(self.SAVE_DIR)
+            torch.save(label, self.LABEL_PATH)
+            # Save final signal_file
+            with open(self.LABEL_TXT_PATH, 'w') as outfile:
+                outfile.write('# H = 1: {0}\n'.format(np.count_nonzero(label.cpu().numpy())))
+                for data_slice in label.cpu().numpy():
+                    np.savetxt(outfile, data_slice, fmt='%-8.4f')
+                    outfile.write('# New z slice\n')
+            
+
+        print(probOverTime.shape)
+        timex = torch.arange(0, burn_in, 1)
+        plt.plot(timex[0::5], probOverTime[0::5], 'o')
+        plt.savefig(self.DISTRIBUTION_PNG)
+
+        return
+
+if __name__ == "__main__":
+    rng_seed = sys.argv[1]
+    print("RAND: ", rng_seed)
+    start = time.time()
+
+    torch.manual_seed(rng_seed)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    burn_in = 1000
+    n_iter = 1
+
+    print("GPU: ", torch.cuda.get_device_name(0))
+    print("NUM ITERATIONS: ", burn_in)
+    print("VOXEL_SIZE: ", Data.VOXEL_SIZE, Data.VOXEL_SIZE, Data.VOXEL_SIZE)
+
+    start = time.time()
+    sampler = GibbsSampler(rng_seed)
+    sampler.run(burn_in, n_iter)
+    end = time.time()
+    print("Time Elapsed:", end - start)
+
+
+
+    
+    
+    
+    
